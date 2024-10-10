750b63d0af33a015dc2f4628fa9b1043f60d4e4f
soomsoom21 <smo16@sunyorange.edu>
2024-10-09 19:34:40 -0700

    Update c_cipher.py

--- src/c_cipher.py
+++ src/c_cipher.py
@@ -3,13 +3,13 @@ logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 def encrypt(email="abc012"):
     """
-    Encrypt the email by shifting 3 letters up by 3 in the ASCII table.
+    Encrypt the email by shifting the first 3 letters up by 3 in the ASCII table.
 
     Args:
         email (str): The email string to encrypt, expected to be 6 characters long, with the first 3 characters as letters and the last 3 as digits.
  
     Returns:
-        str: The encryted email, or an error message if validation fails.   
+        str: The ecrypted email, or an error message if validation fails.   
     """
     output = "" 
     # Input Validation
@@ -21,11 +21,11 @@ def encrypt(email="abc012"):
     #     B = email[3:] (check second half)
     #     enum_flag = A or B
 
-    if len_flag:                         # NOTE: here we provide input validation on length
+    if len_flag:                         # Input validation on length
         output = "Length check failed.\nEmail must be 6 characters long."
         logging.info(output)
         return output        
-    if anum_flag:                        # NOTE: here we provide input validation on alpha/num
+    if anum_flag:                        # Input validation on alphanumeric format
         output = "Alpha-numeric check failed.\nEmail must have 3 letters followed by 3 digits."
         logging.info(output)
         return output     
@@ -35,15 +35,10 @@ def encrypt(email="abc012"):
         
     # Shift first 3 characters (letters) up by 3 in the ASCII table
     for i in range(3):    # NOTE: here we extract and update element at 0 
-    email_lst[0] = chr(email_lst[i] + 3)        # NOTE: here we convert our ASCII into string
+        email_lst[0] = chr(email_lst[i] + 3)        # NOTE: here we convert our ASCII into string
         
-    # COnvert list back into a string
+    # Convert list back into a string
     email_str = ''.join(email_lst)
-    # keep all updates in the retVal (str) variablei
-    # i.e.,
-    #    email_str = " some string updates here "
-    #    email_1 = email_str.strip()
-    #    retVal = email_1
     retVal = email_str
     return retVal 
 
@@ -52,7 +47,7 @@ def decrypt(email="def345"):
     Decrypt the email by shifting first 3 letters down by 3 in the ASCII table 
 
     Args:
-        email (str): The email string to decrypt, expected to be 6 characters long, with the first 3 letters 3 characters as letters and the 3 digits 3 as digits.
+        email (str): The email string to decrypt, expected to be 6 characters long, with the first 3 letters 3 characters as letters and the last 3 as digits.
     Returns:
         str: The decrypted email, or an error message if validation fails.   
     """
@@ -67,11 +62,11 @@ def decrypt(email="def345"):
     #     A = email[:3] (check first half)
     #     B = email[3:] (check second half)
     #     enum_flag = A or B
-    if len_flag:                         # NOTE: here we provide input validation on length
+    if len_flag:  # Input validation on length
         output = "Length check failed.\nEmail must be 6 characters long."
         logging.info(output)
         return output        
-    if anum_flag:                        # NOTE: here we provide input validation on alpha/num
+    if anum_flag: # Input validation on alphanumeric format
         output = "Alpha num check failed.\nEmail must have 3 letters followed by 3 digits."
         logging.info(output)
         return output   
@@ -81,7 +76,7 @@ def decrypt(email="def345"):
     
     # Shift first 3 characters down by 3 in the ASCII table 
     for i in range(3):
-       email_lst[i] = chr(ord(email_lst[i] - 3)
+       email_lst[i] = chr(ord(email_lst[i]) - 3)
     
     #Convert list back into a string
     email_str = ''.join(email_lst)
