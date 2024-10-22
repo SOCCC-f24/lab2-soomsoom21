64bdae7515aa475ef4987277cae59bb52898966b
soomsoom21 <smo16@sunyorange.edu>
2024-10-22 12:49:38 -0700

    Update c_cipher.py

--- src/c_cipher.py
+++ src/c_cipher.py
@@ -5,12 +5,27 @@ def encrypt(email="abc012"):
     email="abc012"
     output = "" 
     len_flag = (len(email) != 6)
-    anum_flag = email[:3] != 'abc' or email[3:] != '012' 
+    anum_flag = email[:3].isalpha() and email[3:] != '012' 
 
-    email_lst = ["a", "b", "c", "0", "1", "2"]
-    # TODO: complete line(s) below, convert EACH new element into a string
+    #email_lst = ["a", "b", "c", "0", "1", "2"]
+    #(1) add space to email
+    e_space = " ".join (email) 
+    #(2) split at space and set to email_lst
+    email_lst = e_space.split(" ")
+    
     new_ascii = ord(email_lst[0]) + 3    
-    email_lst[0] = chr(new_ascii)        
+    email_lst[0] = chr(new_ascii)    
+    new_ascii = ord(email_lst[1]) + 3    
+    email_lst[1] = chr(new_ascii)  
+    new_ascii = ord(email_lst[2]) + 3    
+    email_lst[2] = chr(new_ascii) 
+    new_ascii = ord(email_lst[3]) + 3    
+    email_lst[3] = chr(new_ascii)  
+    new_ascii = ord(email_lst[4]) + 3    
+    email_lst[4] = chr(new_ascii)  
+    new_ascii = ord(email_lst[5]) + 3    
+    email_lst[5] = chr(new_ascii)  
+    
     if len(email_lst) != 6:
         output += "Length check failed"
         logging.info(output)
