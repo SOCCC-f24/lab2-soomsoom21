7ed4ea24aa37fe5887d626e3cf8c69b14f06c356
soomsoom21 <smo16@sunyorange.edu>
2024-10-31 15:42:15 -0700

    Update c_cipher.py

--- src/c_cipher.py
+++ src/c_cipher.py
@@ -13,7 +13,7 @@ def encrypt(email="abc012"):
    ''' email="abc012" '''
     output = "" 
     len_flag = (len(email) != 6)
-    anum_flag = email[:3].isalpha() and email[3:] != '012' 
+    anum_flag = email[3:].isdeccimal() and email[3:] != '012' 
     #email_lst = ["a", "b", "c", "0", "1", "2"]
     #(1) add space to email
     e_space = " ".join (email) 
@@ -33,29 +33,31 @@ def encrypt(email="abc012"):
     new_ascii = ord(email_lst[5]) + 3    
     email_lst[5] = chr(new_ascii)  
     
-if len(email_lst) != 6:
+   if len_flag:
         output += "Length check failed"
         logging.info(output)
-if anum_flag:
+   if not anum_flag:
         output += "Email must have 3 letters followed by 3 digits."
-else:
+   else:
         logging.info(output)
         print(output)     
 
-        email_str = "dbc012"
+           email_str = ".join(email_lst)
    
-retVal = email_str
-print(retVal) 
+   retVal = email_str
+   print(retVal) 
 
-def decrypt(email="def345"): # Shift first 3 characters down by 3 in the ASCII table
-    output = "" 
+def decrypt(email): # Shift first 3 characters down by 3 in the ASCII table
+   # Shift 4  e_lst = email.split
+    e_space = ''.join(email) 
+    e_lst = e_space.split()
     
-    len_flag = len(email) == 6
-    anum_flag = email.isalpha() and email.isdecimal[3:] != '345' 
+    len_flag = len(email) != 6
+    anum_flag = email[:3].isalpha() and email.isdecimal[3:].isdecimal() 
     if len_flag:
              output += "Length check failed"
              logging.info(output)
-    if anum_flag: isalpha() and isdecimal()
+    if not anum_flag: isalpha() and isdecimal()
     new_ascii = ord(email_lst[0]) - 3    
     email_lst[0] = chr(new_ascii)    
     new_ascii = ord(email_lst[1]) - 3    
@@ -68,9 +70,10 @@ def decrypt(email="def345"): # Shift first 3 characters down by 3 in the ASCII t
     email_lst[4] = chr(new_ascii)  
     new_ascii = ord(email_lst[5]) - 3    
     email_lst[5] = chr(new_ascii)  
-    else:
+
+
         logging.info(output)
         print(output)
 
-    retVal = "aef345"
-    print(retVal)
+    retVal = ".join(email_lst)
+    return retVAl
